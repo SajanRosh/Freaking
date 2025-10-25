a6f517287e33012c6997d5f3c6870ef8698fb795
sajanrosh <sajanroshpv@gmail.com>
2025-10-25 19:35:18 +0530

    modified url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import axios from 'axios'
 
 function App() {
-  const url = "freakingrollins-emgagta0fcbfe6fh.canadacentral-01.azurewebsites.net/";
+  const url = "freakingrollins-emgagta0fcbfe6fh.canadacentral-01.azurewebsites.net";
   useEffect(() =>{
     axios.get(`${url}/weatherforecast`).then(res => {
       console.log(res.data)
