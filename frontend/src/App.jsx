21c30f135ae1e100df76ed5f3595befefec9bf63
sajanrosh <sajanroshpv@gmail.com>
2025-10-25 19:34:44 +0530

    changed the api url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 import axios from 'axios'
 
 function App() {
-  const url = "http://localhost:5157/weatherforecast";
+  const url = "freakingrollins-emgagta0fcbfe6fh.canadacentral-01.azurewebsites.net/";
   useEffect(() =>{
-    axios.get(url).then(res => {
+    axios.get(`${url}/weatherforecast`).then(res => {
       console.log(res.data)
     })
   }, [])
